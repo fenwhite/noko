ce72264bfd6569f79e93bb87d52525fe008fce1b
white <white156@qq.com>
2022-01-16 21:07:29 +0800

    整合logback框架

---
+++ src/main/java/com/kinya/neko/controller/NekoController.java
@@ -0,0 +1,15 @@
+package com.kinya.neko.controller;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author ：white
+ * @description：全Controller基类
+ * @date ：2022/1/16
+ */
+
+public class NekoController {
+    static final protected Logger logger = LoggerFactory.getLogger(NekoController.class);
+}
--- src/main/java/com/kinya/neko/error/GlobalExceptionHandler.java
+++ src/main/java/com/kinya/neko/error/GlobalExceptionHandler.java
@@ -2,6 +2,9 @@ package com.kinya.neko.error;
 
 
 import com.kinya.neko.error.exception.NekoException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -16,6 +19,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
  */
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    private final static Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     /**
      * @author: white
@@ -28,5 +32,11 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(NekoException.class)
     public ResponseEntity<ErroResponse> handlerAtRunTime(NekoException ex, WebRequest request){
         return new ResponseEntity(new ErroResponse(ex),HttpStatus.BAD_REQUEST);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        logger.error(ex.toString());
+        return super.handleExceptionInternal(ex, body, headers, status, request);
     }
 }
