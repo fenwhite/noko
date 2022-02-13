5ea0b675248747fc7f7954e816e5777ab3e62e13
white <white156@qq.com>
2022-02-13 23:21:47 +0800

    修复aop bean捕获异常导致致统一异常处理无法被捕获异常问题 附加部分注解说明

--- src/main/java/com/kinya/neko/interceptors/TimerAop.java
+++ src/main/java/com/kinya/neko/interceptors/TimerAop.java
@@ -14,7 +14,27 @@ import org.springframework.stereotype.Component;
  * @date ：2022/1/17
  */
 @Aspect
+/*
+  @AspectJ refers to a style of declaring aspects as regular Java classes annotated with annotations.
+ * To use @AspectJ aspects in a Spring configuration,
+ * you need to enable Spring support for configuring Spring AOP based on @AspectJ aspects
+ * and auto-proxying beans based on whether or not they are advised by those aspects.
+ * To enable @AspectJ support with Java @Configuration,
+ * add the @EnableAspectJAutoProxy annotation, as the following example shows:
+ * <code>
+ * @Configuration
+ * @EnableAspectJAutoProxy
+ * public class AppConfig {
+ *
+ * }
+ * </code>
+ */
 @Component
+/*
+ * Spring Component annotation is used to denote a class as Component.
+ * It means that Spring framework will autodetect these classes for dependency injection
+ * when annotation-based configuration and classpath scanning is used.
+ */
 public class TimerAop {
     private final static Logger logger = LoggerFactory.getLogger(TimerAop.class);
 
@@ -25,11 +45,12 @@ public class TimerAop {
     public Object execAround(ProceedingJoinPoint pj) throws Throwable{
         System.out.println("method name is:"+pj.getSignature());
         System.out.println("aop:around BEGIN:--------");
-        Object retVal = null;
+        Object retVal;
         try {
             retVal = pj.proceed();
         } catch (Throwable throwable) {
-            ;
+            System.out.println("-----------end with exception");
+            throw throwable;
         }
 
         System.out.println("-----------aop:around FINISH");
